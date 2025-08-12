276b596c509e6325b8792c0ca991fb7879733f7a
simon liu <ximeng.liu@anu.edu.au>
2025-08-12 17:26:35 +1000

    Fix bug

--- src/main/java/com/peaknote/demo/controller/AttendeeController.java
+++ src/main/java/com/peaknote/demo/controller/AttendeeController.java
@@ -3,6 +3,7 @@ package com.peaknote.demo.controller;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
